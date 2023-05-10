3a0f059d4f2895d50cc47bbcbe52ce6d637a5e3d
Pavel-Ladanov <p-ladanov@yandex.ru>
2023-05-10 20:21:17 +0300

    add 2 tests in login page add login_page with methods

---
+++ pages/login_page.py
@@ -0,0 +1,13 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+class LoginPage(BasePage):
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, 'wrong url'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "no login form"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "no registration form"
